3b2fd25fc7f307428b623e086f1ca8d27bdaf286
Majid Kargar <majidkargar01@gmail.com>
2023-09-08 06:22:32 +0330

    refactor(script.js): Refactor the file
    Remove 2 unnecessary functions to clean some code.
    
    Add comments.

--- src/js/script.js
+++ src/js/script.js
@@ -1,64 +1,74 @@
+// Importing the "convertFormat" function from the "../helpers/convertFormat.js" file
 import convertFormat from "../helpers/convertFormat.js";
-// variables
-const userInputEl = document.querySelector("#user-input"),
-	copyBtn = document.querySelector("#copy-btn"),
-	convertBtn = document.querySelector("#convert-btn"),
-	newValueBtn = document.querySelector("#new-value"),
-	convertedValueEl = document.querySelector("#converted-value");
+
+// Selecting the HTML element with ID "user-input"
+const userInputEl = document.querySelector("#user-input");
+
+// Selecting the HTML element with ID "copy-btn"
+const copyBtn = document.querySelector("#copy-btn");
+
+// Selecting the HTML element with ID "convert-btn"
+const convertBtn = document.querySelector("#convert-btn");
+
+// Selecting the HTML element with ID "new-value"
+const newValueBtn = document.querySelector("#new-value");
+
+// Selecting the HTML element with ID "converted-value"
+const convertedValueEl = document.querySelector("#converted-value");
+
+// Initializing the userInputValue variable with an empty string
 let userInputValue = "";
 
-// copy button
-copyEvents(copyBtn);
+// Attaching a "copyEventListener" event listener to the copyBtn element
+copyEventListener(copyBtn);
 
-// converted Value El
-copyEvents(convertedValueEl);
+// Attaching a "copyEventListener" event listener to the convertedValueEl element
+copyEventListener(convertedValueEl);
 
-// convert button
+// Attaching a "click" event listener to the convertBtn element
 convertBtn.addEventListener("click", () => {
 	if (userInputEl.value.length > 0) {
-		getUserInput();
+		// Assigning the value of userInputEl to userInputValue
+		userInputValue = userInputEl.value;
+
+		// Calling the convertFormat function with userInputValue and assigning its return value to the value property of convertedValueEl
+		convertedValueEl.value = convertFormat(userInputValue);
 	} else {
+		// Displaying an alert if no value is entered in userInputEl
 		alert("لطفا یک مقدار وارد کنید");
 	}
 });
 
-// new value
+// Attaching a "click" event listener to the newValueBtn element
 newValueBtn.addEventListener("click", () => {
+	// Clearing the value of userInputEl and convertedValueEl
 	userInputEl.value = "";
 	convertedValueEl.value = "";
+
+	// Removing the "copied" class from convertedValueEl
 	convertedValueEl.classList.remove("copied");
 });
 
-// TITLE: get user input
-// gets user input and puts it in userInputValue variable
-function getUserInput() {
-	userInputValue = userInputEl.value;
-	convertedValueEl.value = convertFormat(userInputValue);
-}
-
-// TITLE: copy text
-function copyText() {
-	navigator.clipboard.writeText(convertedValueEl.value);
-	convertedValueEl.classList.add("copied");
-}
-
-// TITLE: copy events
-// creates an event Listener for click and if
-// convertedValueEl.value is not empty calls copyText() function
-// parameters:
-// clickedElement = the element that has been clicked
-function copyEvents(clickedElement) {
+// Defining the copyEventListener function that attaches a "click" event listener to the clickedElement
+const copyEventListener = (clickedElement) => {
 	clickedElement.addEventListener("click", () => {
 		if (convertedValueEl.value.length > 0) {
-			copyText();
+			// Copying the value of convertedValueEl to the clipboard
+			navigator.clipboard.writeText(convertedValueEl.value);
+
+			// Adding the "copied" class to convertedValueEl
+			convertedValueEl.classList.add("copied");
 		} else {
+			// Displaying an alert if no value is converted in convertedValueEl
 			alert("لطفا ابتدا تبدیل کنید");
 		}
 	});
-}
+};
 
-// Reset form
+// Selecting the HTML element with ID "input-box"
 const formEl = document.querySelector("#input-box");
+
+// Attaching a "submit" event listener to the formEl element
 formEl.addEventListener("submit", (e) => {
 	e.preventDefault();
 });
